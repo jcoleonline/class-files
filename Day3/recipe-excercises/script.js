84459664f9daaf08175f62f1de81f9eb71686d76
Ramon Santana <jcoleonlinellc@gmail.com>
2023-01-03 20:07:39 -0600

    1st repo

---
+++ Day3/recipe-excercises/script.js
@@ -0,0 +1,12 @@
+// Given a string of characters as input, write a function that returns it with the characters reversed.
+
+const characters = ["a","b","c","d"];
+
+function reverse(){    
+    for(let i=3; i < 4; i--){
+        let character = characters[i];
+
+        console.log(character) }
+    }
+
+    
